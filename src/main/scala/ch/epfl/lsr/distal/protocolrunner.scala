5f4e57c65c169c6b0d1ee072684601f0220ca9ca
Martin Biely <martin.biely@epfl.ch>
2012-09-14 14:22:05 +0200

    timer and runner

---
+++ src/main/scala/ch/epfl/lsr/distal/protocolrunner.scala
@@ -0,0 +1,66 @@
+package ch.epfl.lsr.distal
+
+import java.net.InetSocketAddress
+
+object DSLProtocolRunner { 
+  def main(args :Array[String]) { 
+    // first try ID on argument 
+    if(args.size > 0) { 
+
+      // TODO: take arg(1) as optional file? 
+      val id = args(0)
+      createProtocol(id, ProtocolsConf.getClazz(id)).start
+
+    } else { 
+
+      // then try local Socketaddress as per dsl.local.address SystemProperty
+      val prop = System.getProperty("dsl.local.address")
+      if(prop!=null) { 
+	val Array(host,port) = prop.split(":")
+	val entries = ProtocolsConf.getEntriesForSocketAddress(new InetSocketAddress(host, port.toInt))
+	entries.map { e => createProtocol(e.id, e.clazz, false) } map {
+	  p => if(p!=null) p.start 
+	}
+
+      } else { 
+	
+	println("Not found any protocols to start. ")
+	
+      }
+    }
+  }
+
+  private def getStringConstructor(clazz :Class[_]) :Option[java.lang.reflect.Constructor[_]] = { 
+    try { 
+      Some(clazz.getConstructor(classOf[String]))
+    } catch { 
+      case e :NoSuchMethodException =>None
+    }
+  }
+
+  def createProtocol[_](ID :String, clazz :Option[Class[_]], failOnNone :Boolean = true) : DSLProtocol = { 
+    clazz match { 
+      case Some(clazz) => 
+	try { 
+	  getStringConstructor(clazz) match { 
+	    case Some(cons) =>
+	      cons.newInstance(ID).asInstanceOf[DSLProtocol]
+	    case None =>
+	      try { 
+		clazz.newInstance.asInstanceOf[DSLProtocol]
+	      } catch { 
+		case e :NoSuchMethodException =>
+		  throw new Exception("Cannot create protocol from ID "+ID+": class does not have suitable constructors")
+	      }
+	  }
+	}
+      case None => 
+	if(failOnNone) { 
+	  throw new Exception("Cannot create protocol from ID "+ID+": no class set in config")
+	} else { 
+	  System.err.println("WARNING: Skipping creation of ID: not class set in config."); null
+	}
+    }
+  }
+
+}
---
+++ src/main/scala/ch/epfl/lsr/distal/timer.scala
@@ -0,0 +1,19 @@
+package ch.epfl.lsr.distal
+
+import ch.epfl.lsr.netty.util.{ Timer => TimerImpl }
+import java.util.concurrent.TimeUnit
+import ch.epfl.lsr.netty.protocol._
+
+case class Duration(amount :Int, unit :TimeUnit)
+
+class ProtoDuration(amount :Int) { 
+  def apply(unit :TimeUnit) = Duration(amount,unit)
+}
+
+trait ImplicitDurations { 
+  import language.implicitConversions
+  
+  implicit def int2ProtoDuration(i :Int) = new ProtoDuration(i)
+}
+
+// TIMER is implemented using AFTER keyword in DSL.
